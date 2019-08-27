909d924efed0df97ceecf5700ad5f29a64299a2c
Blair McLaughlin <Blairmclaughlin89@gmail.com>
2019-08-26 23:27:05 -0500

    solved

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+if (a.length % 2 === 0) {
+        return a[Math.floor((a.length - 1) / 2)];
+    }
+     return a[Math.round((a.length - 1) / 2)];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+//console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
