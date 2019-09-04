f2db29a4924c1c324711fa1d26242cc3e645c365
Tangent314 <blairmclaughlin@Blairs-MacBook-Pro.local>
2019-09-03 21:34:16 -0500

    solved using array.push

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,13 @@
 function arrayConversion(inputArray: number[]): number {
-
+let arr = [];
+let arr2 = [];
+for (let i = 0; i < inputArray.length - 1; i+=2) {
+    arr.push(inputArray[i] + inputArray[i+1])
+}
+for (let j = 0; j < arr.length - 1; j+=2) {
+    arr2.push(arr[j] * arr[j+1]);
+}
+return arr2[0]+ arr2[1];
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
